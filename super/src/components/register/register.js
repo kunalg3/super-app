b69ef08aa4534b0f102cf00557d693fb0cb76a91
kunalg3 <110518293+kunalg3@users.noreply.github.com>
2024-01-20 16:31:36 +0530

    folder setup

---
+++ super/src/components/register/register.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function register() {
+  return (
+    <div>registercomponent</div>
+  )
+}
+
+export default register
---
+++ super/src/pages/Register/registerpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import register from '../../components/register/register'
+
+function registerpage() {
+  return (
+    <div>
+      <register/>
+    </div>
+  )
+}
+
+export default registerpage
